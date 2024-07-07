47fbb6e89387067045c439c2c4ba6ecf7b440e6d
Andrew Harris <Andrew.Harris@torontomu.ca>
2024-07-07 15:20:23 -0400

    fixed postgresql locally

--- config.py
+++ config.py
@@ -1,22 +1,19 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
-    DATABASE_URL = os.getenv('DATABASE_URL')
-    if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
-        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
-    SQLALCHEMY_DATABASE_URI = DATABASE_URL or 'sqlite:///app.db'
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    
+    # Update the SQLALCHEMY_DATABASE_URI to use PostgreSQL
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://aharris:Ernskyk3!@localhost/showbase')
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = 'uploads/receipts'
     ALLOWED_EXTENSIONS = {'pdf', 'jpeg', 'jpg', 'png'}
-    DEBUG = os.getenv('FLASK_DEBUG', 'False').lower() in ['true', '1', 't']
-
+    
     # Email settings
-    MAIL_SERVER = os.getenv('MAIL_SERVER', 'smtp.googlemail.com')
-    MAIL_PORT = int(os.getenv('MAIL_PORT', 587))
-    MAIL_USE_TLS = bool(os.getenv('MAIL_USE_TLS', True))
-    MAIL_USERNAME = os.getenv('EMAIL_USER')
-    MAIL_PASSWORD = os.getenv('EMAIL_PASS')
-    MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER', MAIL_USERNAME)
-
-print(f"Configured DATABASE_URL: {Config.SQLALCHEMY_DATABASE_URI}")
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+    MAIL_DEFAULT_SENDER = os.environ.get('EMAIL_USER')
