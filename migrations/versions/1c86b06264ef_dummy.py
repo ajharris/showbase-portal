dec371d6e4119ff7aa5246d947d54fd553046cc1
Andrew Harris <Andrew.Harris@torontomu.ca>
2024-07-17 13:11:26 -0400

    fixed database

---
+++ migrations/versions/1c86b06264ef_dummy.py
@@ -0,0 +1,13 @@
+"""Dummy migration to resolve missing revision issue"""
+
+# revision identifiers, used by Alembic.
+revision = '1c86b06264ef'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass
